8f8ac4857b4cac8ef0dae6317859c5eb468a9466
GoatTokens <99522835+GoatTokens@users.noreply.github.com>
2022-02-11 23:35:51 -0600

    Add files via upload

---
+++ GoatTokenDiscordGen.py
@@ -0,0 +1,12 @@
+Telegram @GoatTokens
+I have a working discord token gen but its not for sale
+instead 
+Greatest of all tokens: 
+    https://GoatTokens.sellix.io 
+ Minimum order is only $0.70
+       Each HQ Token $0.07
+Tested and each do around 50 DMs!
+All come with name+PFP!
+
+
+GOATTOKENS.py
